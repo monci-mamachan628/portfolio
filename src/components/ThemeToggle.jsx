caef2ad97b25db9a7913ec222d17ea5b1ccd98b6
simi <simimole777@gmail.com>
2026-08-29 19:32:05 +0100

    Default dark

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,17 +4,18 @@ import { useState, useEffect } from 'react';
 import { cn } from '../lib/utils';
 
 export const ThemeToggle = () => {
-    const [IsDarkMode, setIsDarkMode] = useState(false);
+    const [IsDarkMode, setIsDarkMode] = useState(true);
 
     useEffect(() => {
         const storedTheme = localStorage.getItem('theme');
-        if (storedTheme === 'dark') {
-            setIsDarkMode(true);
-            document.documentElement.classList.add('dark');
-        } else {
-            localStorage.setItem('theme', 'light');
+
+        if (storedTheme === 'light') {
             setIsDarkMode(false);
             document.documentElement.classList.remove('dark');
+        } else {
+            setIsDarkMode(true);
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
         }
     }, []);
 
