740ff2d3807dca619b283d6f5db439e3527ae39f
simi <simimole777@gmail.com>
2026-08-29 19:11:58 +0100

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
